83e88937747897c1b3187f3f80cee39863571fc5
mshenawy <m.alshenawy91@gmail.com>
2017-07-30 15:32:28 +0200

    maintain issue in send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,6 +113,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
     :address        => "smtp.mailgun.org",
+    :domain         => ENV['domain'],
     :user_name      => ENV['username'],
     :password       => ENV['password'],
     :authentication => :plain,
