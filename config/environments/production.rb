fc1ddd5c0c6e5bcbc37b50812e685a52fab76782
mshenawy <m.alshenawy91@gmail.com>
2017-07-30 15:20:22 +0200

    mentain issue in send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,7 +113,6 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
     :address        => "smtp.mailgun.org",
-    :domain         => ENV['domain'],
     :user_name      => ENV['username'],
     :password       => ENV['password'],
     :authentication => :plain,
